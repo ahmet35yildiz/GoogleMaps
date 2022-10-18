e6e82de741379129a9eb513fb6678cbe4a06d6f2
Ahmet YILDIZ <ahmet35yildiz@gmail.com>
2022-10-18 15:57:21 +0300

    GoogleMaps

---
+++ GoogleMapsExample/app/src/main/java/com/example/googlemapsexample/MapsActivity.kt
@@ -0,0 +1,37 @@
+package com.example.googlemapsexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.googlemapsexample.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val location = LatLng(38.4, 27.15)
+        mMap.addMarker(MarkerOptions().position(location).title("İzmir"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,10f))
+        mMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
+    }
+}
